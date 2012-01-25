31810cd066b5e475c0137af9976a71ae8bbc071d
Leevi Graham <leevi@newism.com.au>
2012-01-25 15:43:50 +1100

    Initial commit

---
+++ config.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Config file for NSM Gravatar
+ *
+ * @package			NSMGravatar
+ * @version			0.0.1
+ * @author			Leevi Graham <http://leevigraham.com>
+ * @copyright 		Copyright (c) 2007-2010 Newism <http://newism.com.au>
+ * @license 		Commercial - please see LICENSE file included with this distribution
+ * @link			http://ee-garage.com/nsm-gravatar
+ */
+
+if(!defined('NSM_GRAVATAR_VERSION')) {
+	define('NSM_GRAVATAR_VERSION', '0.0.1');
+	define('NSM_GRAVATAR_NAME', 'NSM Gravatar');
+	define('NSM_GRAVATAR_ADDON_ID', 'nsm_gravatar');
+}
+
+$config['name'] 	= NSM_GRAVATAR_NAME;
+$config["version"] 	= NSM_GRAVATAR_VERSION;
+
+// $config['nsm_addon_updater']['versions_xml'] = 'http://ee-garage.com/nsm-gravatar';
